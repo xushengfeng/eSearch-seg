b574d42e761937d213553b8c489cb1741cd27a44
xushengfeng <xushengfeng_zg@163.com>
2025-07-03 00:43:21 +0800

    测试 更新代码

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const x = require("../");
+const ort = require("onnxruntime-node");
 
 start();
 
@@ -7,20 +8,25 @@ async function start() {
         segPath: "./m/398x224softmax.onnx",
         dev: true,
         node: true,
+        ort,
+        invertOpacity: true,
+        shape: [256, 144],
     });
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = "../x.webp";
     img.onload = () => {
-        let canvas = document.createElement("canvas");
+        const canvas = document.createElement("canvas");
         canvas.width = img.width;
         canvas.height = img.height;
         canvas.getContext("2d").drawImage(img, 0, 0);
-        x.seg(canvas.getContext("2d").getImageData(0, 0, img.width, img.height)).then((data) => {
+        x.seg(
+            canvas.getContext("2d").getImageData(0, 0, img.width, img.height),
+        ).then((data) => {
             xx.width = data.width;
             xx.height = data.height;
             xx.getContext("2d").putImageData(data, 0, 0);
         });
     };
 }
-let xx = document.createElement("canvas");
+const xx = document.createElement("canvas");
 document.body.append(xx);
--- test/test1.js
+++ test/test1.js
@@ -1,23 +1,28 @@
 const x = require("../");
+const ort = require("onnxruntime-node");
 
 start();
 
 async function start() {
     await x.init({
         segPath: "./m/398x224softmax.onnx",
+        ort,
+        invertOpacity: true,
+        shape: [256, 144],
+        threshold: 0.6,
     });
-    let cameraStream = await navigator.mediaDevices.getUserMedia({
+    const cameraStream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: true,
     });
     v.srcObject = cameraStream;
     v.play();
 }
-let v = document.createElement("video");
-let i = document.createElement("canvas");
+const v = document.createElement("video");
+const i = document.createElement("canvas");
 document.body.append(i);
 i.style.filter = "blur(60px)";
-let out = document.createElement("canvas");
+const out = document.createElement("canvas");
 document.body.append(out);
 out.style.position = "relative";
 
@@ -26,12 +31,14 @@ function draw() {
     i.width = v.videoWidth;
     i.height = v.videoHeight;
     canvasCtx.drawImage(v, 0, 0, i.width, i.height);
-    x.seg(i.getContext("2d").getImageData(0, 0, i.width, i.height)).then((data) => {
-        out.width = data.width;
-        out.height = data.height;
-        out.getContext("2d").putImageData(data, 0, 0);
-        out.style.top = `-${i.height}px`;
-    });
+    x.seg(i.getContext("2d").getImageData(0, 0, i.width, i.height)).then(
+        (data) => {
+            out.width = data.width;
+            out.height = data.height;
+            out.getContext("2d").putImageData(data, 0, 0);
+            out.style.top = `-${i.height}px`;
+        },
+    );
     setTimeout(() => {
         draw();
     }, 10);
